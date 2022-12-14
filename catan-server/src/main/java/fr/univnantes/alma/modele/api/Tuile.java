90b9b56b7520b7d2d0ec6acd8a4f4385f78132ea
Mathis Dupuis <mathis.dupuis@etu.univ-nantes.fr>
2022-12-14 21:31:50 +0100

    ajout javaDoc Tuile, completion methods.adoc

--- catan-server/src/main/java/fr/univnantes/alma/modele/api/Tuile.java
+++ catan-server/src/main/java/fr/univnantes/alma/modele/api/Tuile.java
@@ -1,6 +1,35 @@
 package fr.univnantes.alma.modele.api;
 
+import fr.univnantes.alma.modele.api.enums.Ressource;
+import fr.univnantes.alma.modele.api.enums.Couleur;
+
+import java.util.List;
+import java.util.Map;
+
 public interface Tuile {
 
-    Intersection[] getAllIntersections();
+    /**
+     * Renvoie la liste des intersections autour de la tuile.
+     */
+    List<Intersection> getIntersections();
+
+    /**
+     * Renvoie la ressource de la tuile.
+     */
+    Ressource getRessource();
+
+    /**
+     * Renvoie un dictionnaire attribuant à une `couleur` un couple `(Ressource,Integer)` indiquant le nombre de ressources
+     * à distribuer au joueur de cette `couleur`.
+     *
+     * Récupère les constructions des intersections autour de la tuile.
+     * Pour chacune de ces constructions, récupérer sa couleur et sa `puissance`, et modifier le dictionnaire ainsi :
+     * `result[couleur][ressource] += puissance`, `ressource` étant la ressource de la tuile.
+     */
+    Map<Couleur, Map<Ressource,Integer>> distribuerRessources();
+
+    /**
+     * Renvoie la valeur du jeton de la tuile.
+     */
+    int getValeurJeton();
 }
--- catan-server/src/main/java/fr/univnantes/alma/modele/impl/TuileImpl.java
+++ catan-server/src/main/java/fr/univnantes/alma/modele/impl/TuileImpl.java
@@ -2,29 +2,40 @@ package fr.univnantes.alma.modele.impl;
 
 import fr.univnantes.alma.modele.api.Intersection;
 import fr.univnantes.alma.modele.api.Tuile;
+import fr.univnantes.alma.modele.api.enums.Couleur;
 import fr.univnantes.alma.modele.api.enums.Ressource;
+
+import java.util.List;
+import java.util.Map;
 
 public class TuileImpl implements Tuile {
 
-    private Intersection[] listIntersections;
+    private List<Intersection> listIntersections;
     private final Ressource ressource;
     private int valeurJeton;
 
-    public TuileImpl(Intersection[] listIntersections, Ressource ressource, int valeurJeton) {
+    public TuileImpl(List<Intersection> listIntersections, Ressource ressource, int valeurJeton) {
         this.listIntersections = listIntersections;
         this.ressource = ressource;
         this.valeurJeton = valeurJeton;
     }
 
     @Override
-    public Intersection[] getAllIntersections() {
-        return listIntersections;
+    public List<Intersection> getIntersections() {
+        return this.listIntersections;
     }
 
+    @Override
     public Ressource getRessource() {
         return ressource;
     }
 
+    @Override
+    public Map<Couleur, Map<Ressource, Integer>> distribuerRessources() {
+        return null;
+    }
+
+    @Override
     public int getValeurJeton() {
         return valeurJeton;
     }
