14428c62f82d16b13b1b8b1dab41ba19cbb64103
Mathis Dupuis <mathis.dupuis@etu.univ-nantes.fr>
2022-12-14 20:41:59 +0100

    ajout javaDoc Construction

--- catan-server/src/main/java/fr/univnantes/alma/modele/api/Construction.java
+++ catan-server/src/main/java/fr/univnantes/alma/modele/api/Construction.java
@@ -3,7 +3,16 @@ package fr.univnantes.alma.modele.api;
 import fr.univnantes.alma.modele.api.enums.Couleur;
 
 public interface Construction {
-    public abstract Couleur getCouleur();
 
-    public abstract int getPuissance();
+    /**
+     * Retourne la couleur de la construction.
+     * @return la Couleur.
+     */
+    Couleur getCouleur();
+
+    /**
+     * Retourne la puissance de la construction. 1 pour une colonie, 2 pour une ville.
+     * @return un int, la puissance.
+     */
+    int getPuissance();
 }
