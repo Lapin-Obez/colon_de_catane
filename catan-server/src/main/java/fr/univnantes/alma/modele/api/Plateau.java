9a1375150be5824fa967f07a5026eb8f41a20bc8
Mathis Dupuis <mathis.dupuis@etu.univ-nantes.fr>
2022-12-14 21:24:28 +0100

    Ajout javaDoc Plateau

--- catan-server/src/main/java/fr/univnantes/alma/modele/api/Plateau.java
+++ catan-server/src/main/java/fr/univnantes/alma/modele/api/Plateau.java
@@ -1,9 +1,57 @@
 package fr.univnantes.alma.modele.api;
 
 import fr.univnantes.alma.modele.api.enums.Couleur;
+import fr.univnantes.alma.modele.api.enums.Ressource;
+import fr.univnantes.alma.modele.api.exceptions.ImpossibleBuildException;
+
+import java.util.List;
+import java.util.Map;
 
 public interface Plateau {
     int getLongueurRoute(Couleur couleur);
     void deplacerVoleur(int idTuile);
-    void distribuerRessources(int valeurJeton);
+
+    /**
+     * Construit une colonie de la `couleur` donnée à l'intersection donnée.
+     * @param couleur la Couleur.
+     * @param idIntersection un int, l'id de l'intersection.
+     * @throws IllegalArgumentException si l'intersection est invalide.
+     * @throws ImpossibleBuildException si l'intersection est déjà occupée ou
+     *                                  si elle n'est pas à deux de distance minimum d'une autre colonie.
+     */
+    void construireColonie(Couleur couleur, int idIntersection) throws IllegalArgumentException, ImpossibleBuildException;
+
+    /**
+     * Améliore une colonie au niveau de ville. Cette colonie se trouve à l'intersection d'id `idIntersection`.
+     * @param couleur la Couleur.
+     * @param idIntersection un int, l'id de l'intersection.
+     * @throws IllegalArgumentException si l'intersection est invalide.
+     * @throws ImpossibleBuildException s'il n'y a pas de colonie sur l'intersection ou
+     *                                  si la colonie n'appartient pas au joueur.
+     */
+    void construireVille(Couleur couleur, int idIntersection) throws IllegalArgumentException, ImpossibleBuildException;
+
+    /**
+     * Construit une route de la couleur donnée entre deux intersections données.
+     * @param couleur la Couleur.
+     * @param intersectionA un int, l'id de la première intersection.
+     * @param intersectionB un int, l'id de la deuxième intersection.
+     */
+    void construireRoute(Couleur couleur, int intersectionA, int intersectionB) throws IllegalArgumentException, ImpossibleBuildException;
+
+    /**
+     * Renvoie un dictionnaire attribuant à une `couleur` un couple `(Ressource,Integer)` indiquant le nombre de ressources
+     * à distribuer au joueur de cette `couleur`.
+     * @param valeurJeton unt int, la valeur du jeton.
+     * @throws IllegalArgumentException si la `valeurJeton` ne correspond à aucune tuile.
+     */
+    Map<Couleur, Map<Ressource,Integer>> distribuerRessources(int valeurJeton) throws IllegalArgumentException;
+
+    /**
+     * Change le voleur de tuile et renvoie quelles couleurs sont sur cette tuile.
+     * @param idTuile un int, l'id de la tuile.
+     * @return la liste des Couleur présentes sur la tuile volée.
+     * @throws IllegalArgumentException si la tuile n'est pas valide.
+     */
+    List<Joueur> jouerVoleur(int idTuile) throws IllegalArgumentException;
 }
--- catan-server/src/main/java/fr/univnantes/alma/modele/impl/PlateauImpl.java
+++ catan-server/src/main/java/fr/univnantes/alma/modele/impl/PlateauImpl.java
@@ -1,10 +1,14 @@
 package fr.univnantes.alma.modele.impl;
 
 import fr.univnantes.alma.modele.api.Intersection;
+import fr.univnantes.alma.modele.api.Joueur;
 import fr.univnantes.alma.modele.api.Plateau;
 import fr.univnantes.alma.modele.api.Tuile;
 import fr.univnantes.alma.modele.api.enums.Couleur;
+import fr.univnantes.alma.modele.api.enums.Ressource;
+import fr.univnantes.alma.modele.api.exceptions.ImpossibleBuildException;
 
+import java.util.List;
 import java.util.Map;
 
 public class PlateauImpl implements Plateau {
@@ -17,6 +21,30 @@ public class PlateauImpl implements Plateau {
     public PlateauImpl() {
     }
 
+    @Override
+    public void construireColonie(Couleur couleur, int idIntersection) throws IllegalArgumentException, ImpossibleBuildException {
+
+    }
+
+    @Override
+    public void construireVille(Couleur couleur, int idIntersection) throws IllegalArgumentException, ImpossibleBuildException {
+
+    }
+
+    @Override
+    public void construireRoute(Couleur couleur, int intersectionA, int intersectionB) throws IllegalArgumentException, ImpossibleBuildException {
+
+    }
+
+    @Override
+    public Map<Couleur, Map<Ressource, Integer>> distribuerRessources(int valeurJeton) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public List<Joueur> jouerVoleur(int idTuile) throws IllegalArgumentException {
+        return null;
+    }
 
     @Override
     public int getLongueurRoute(Couleur couleur) {
@@ -25,11 +53,6 @@ public class PlateauImpl implements Plateau {
 
     @Override
     public void deplacerVoleur(int idTuile) {
-
-    }
-
-    @Override
-    public void distribuerRessources(int valeurJeton) {
 
     }
 }
