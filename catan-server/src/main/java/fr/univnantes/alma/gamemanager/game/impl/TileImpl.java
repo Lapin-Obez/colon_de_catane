1bbfe259dbcb287274d6522b5c9ba36d7fd0bf33
Mathis Dupuis <mathis.dupuis@etu.univ-nantes.fr>
2022-12-19 18:01:47 +0100

    Réécriture de distributeResources() pour qu'elle retourne la valeur demandée

--- catan-server/src/main/java/fr/univnantes/alma/gamemanager/game/impl/TileImpl.java
+++ catan-server/src/main/java/fr/univnantes/alma/gamemanager/game/impl/TileImpl.java
@@ -28,28 +28,26 @@ public class TileImpl implements Tile {
 
     @Override
     public Resource getResource() {
-        return resource;
+        return this.resource;
     }
 
     @Override
     public Map<Color, Map<Resource, Integer>> distributeResources() {
         Map<Color, Map<Resource, Integer>> distributeResources = new HashMap<>();
-        for(Intersection i:this.listIntersections){
-            if(i.getConstruction().getClass()== Colony.class){
-                Map<Resource,Integer> res = new HashMap<Resource,Integer>();
-                res.put(this.resource,1);
-                distributeResources.put(i.getConstruction().getColor(),res);
-            }else if(i.getConstruction().getClass()== City.class){
-                Map<Resource,Integer> res = new HashMap<Resource,Integer>();
-                res.put(this.resource,2);
-                distributeResources.put(i.getConstruction().getColor(),res);
-            }
+
+        for(Intersection inter : this.listIntersections) {
+            Color color = inter.getConstruction().getColor();
+            Map<Resource, Integer> resources = distributeResources.getOrDefault(color, new HashMap<>());
+            resources.put(this.resource,
+                resources.getOrDefault(this.resource, 0) + inter.getConstruction().getPower());
+            distributeResources.put(color, resources);
         }
+
         return distributeResources;
     }
 
     @Override
     public int getTokenValue() {
-        return tokenValue;
+        return this.tokenValue;
     }
 }
